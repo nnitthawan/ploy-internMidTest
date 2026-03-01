c8368be19423e82770c0d85a573fd80c34a84728
nnitthawan <nitthawan.p@kkumail.com>
2026-03-01 15:40:51 +0700

    updatePageWelcome

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
+use App\Models\Books;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('users.books');
+// });
 
 Route::resource('books', BooksController::class);
+
+// แก้เป็น
+
+Route::get('/', function () {
+    $books = Books::all(); 
+    $categories = Category::all();
+
+    return view('users.books', compact('books', 'categories'));
+});
